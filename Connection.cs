ea5dd6f0ddc89b2eb8811e0eb1a3735bfc42dfbe
Jake Gough <super.jake@hotmail.co.uk>
2018-12-13 14:15:18 +0000

    Add files via upload

---
+++ Connection.cs
@@ -0,0 +1,7 @@
+﻿namespace RosterWebApp
+{
+    public static class Connection
+    {
+        public static string ConnString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Source\\RosterWebApp\\RosterWebApp\\RosterWebAppDB.mdf;Integrated Security=True";
+    }
+}
